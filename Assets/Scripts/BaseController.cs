375338a9d3831e619142906af94f7b33225b7d71
Gabriel <g.larriuz@gmail.com>
2016-07-20 08:34:36 -0400

    Version 0.1
    This is the prototype for the Game laVRynth for the Gear VR. There is a
    laberynth that works with the GearVR.

---
+++ Assets/Scripts/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class BaseController : MonoBehaviour {
+
+    public Transform OVRCamera;
+       
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Quaternion camRot = OVRCamera.rotation;
+        transform.rotation = camRot;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x-OVRCamera.parent.parent.rotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
+
+        
+
+    }
+}
