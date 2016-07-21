8dcb9321de083c5fbab6681098eee3ac8afba72a
Gabriel <g.larriuz@gmail.com>
2016-07-21 09:33:22 -0400

    Ball generator, gameplay tweaks and visual guides

---
+++ Assets/BallController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+    AudioSource audioSource;
+    Rigidbody rgb;
+    bool isMoving;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        rgb = GetComponent<Rigidbody>();
+        isMoving = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mag = rgb.velocity.magnitude;
+
+        if (isMoving)
+        {
+            audioSource.pitch = mag;
+        }
+        else
+        if (!isMoving && mag > 0)
+        {
+            audioSource.Play();
+            audioSource.pitch = mag;
+            isMoving = true;
+        }
+        else
+        if (isMoving  &&  mag==0) {
+            audioSource.Stop();
+            isMoving = false;
+        }
+        
+        
+	}
+}
--- Assets/Scripts/BaseController.cs
+++ Assets/Scripts/BaseController.cs
@@ -5,19 +5,43 @@ using UnityEngine.VR;
 public class BaseController : MonoBehaviour {
 
     public Transform OVRCamera;
+    public float xMult;
+    public float zMult;
+    float lastRot;
+    Vector3 initPos;
+    
        
 	// Use this for initialization
 	void Start () {
-	
-	}
+       
+        initPos = transform.position;
+
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        Quaternion camRot = OVRCamera.rotation;
-        transform.rotation = camRot;
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x-OVRCamera.parent.parent.rotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
+        float xEuAngle = OVRCamera.eulerAngles.x - OVRCamera.parent.parent.rotation.eulerAngles.x;
+        float zEuAngle = OVRCamera.eulerAngles.z - OVRCamera.parent.parent.rotation.eulerAngles.z;
 
-        
+        transform.rotation = Quaternion.Euler(xEuAngle * xMult , 0 , zEuAngle * zMult);
+
+
+        transform.position = initPos;
+        /*
+
+        if (transform.rotation.x != lastRot && transform.rotation.x - lastRot >= 0)
+        {
+            transform.Translate(transform.up * -yTransform);
+        }
+
+
+        if (transform.rotation.y != lastRot && transform.rotation.x - lastRot <= 0)
+        {
+            transform.Translate(transform.up * yTransform);
+
+        }
+        lastRot = transform.rotation.eulerAngles.x;*/
+
 
     }
 }
