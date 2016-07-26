5bf4a030869dec5f98aa48023eb804c445a1f2bb
gablar <g.larriuz@gmail.com>
2016-07-26 06:27:34 -0400

    Commit from a different machine

---
+++ Assets/Prefabs/Ball/BallGeneratorController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallGeneratorController : MonoBehaviour {
+    public Transform ball;
+	// Use this for initialization
+	void Start () {
+       //GameObject ball =  GameObject.Find("ball");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            Instantiate(ball,transform.position,Quaternion.identity);
+        }
+    }
+}
