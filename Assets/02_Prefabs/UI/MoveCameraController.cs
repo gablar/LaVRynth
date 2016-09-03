c8f77d5303fe763b08a50ca0d44ad4d46c7a6bbe
gablar <g.larriuz@gmail.com>
2016-09-03 11:58:42 -0400

    camera removed eventremoved

---
+++ Assets/02_Prefabs/UI/MoveCameraController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCameraController : MonoBehaviour {
+
+    public Material unSelected;
+    public Material selected;
+    MeshRenderer mRend;
+
+    public Transform cameraRig;
+    public float yMovement = 1;
+    public bool up;
+    // Use this for initialization
+    void Start()
+    {
+        mRend = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+
+    public void OnPointerEnter()
+    {
+        Debug.Log("Arrow OnPointerEnter");
+        mRend.material = selected;
+    }
+
+    public void OnPointerExit()
+    {
+        mRend.material = unSelected;
+    }
+
+    public void OnSubmit()
+    {   if (up)
+        {
+            float newHeight = PlayerPrefs.GetFloat("CameraHeight") + yMovement;
+            PlayerPrefs.SetFloat("CameraHeight", newHeight);
+            cameraRig.transform.position = new Vector3(cameraRig.transform.position.x,
+                                                        cameraRig.transform.position.y + newHeight,
+                                                        cameraRig.transform.position.z);
+        }
+        else {
+            float newHeight = PlayerPrefs.GetFloat("CameraHeight") - yMovement;
+            PlayerPrefs.SetFloat("CameraHeight", newHeight);
+            cameraRig.transform.position = new Vector3(cameraRig.transform.position.x,
+                                                        cameraRig.transform.position.y + newHeight,
+                                                        cameraRig.transform.position.z);
+        }
+
+    }
+}
