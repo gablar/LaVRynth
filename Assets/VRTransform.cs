63d95caf734eb9376269666f3a45ce1f0112f7f6
gablar <g.larriuz@gmail.com>
2016-09-21 13:56:52 -0400

    Revert "Downloaded stuff that broke the project. Reverting to last commit"
    This reverts commit 69ae7fb5511bb23162273a38fcdd06ebcc16b138.

--- Assets/VRTransform.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.VR;
-using System.Collections;
-
-public class VRTransform : MonoBehaviour {
-	void Update () {
-        transform.localRotation = InputTracking.GetLocalRotation(VRNode.CenterEye) * Quaternion.Euler(45, 0, 0);
-	} 
-}
