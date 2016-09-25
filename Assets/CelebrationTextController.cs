201d045606f4f6a825e1e755b8440e972b6d7a8c
gablar <g.larriuz@gmail.com>
2016-09-25 13:30:09 -0400

    polished several laberinths

--- Assets/CelebrationTextController.cs
+++ Assets/CelebrationTextController.cs
@@ -21,4 +21,5 @@ public class CelebrationTextController : MonoBehaviour {
         transform.GetChild(1).gameObject.SetActive(true);
 
     }
+
 }
--- Assets/EndLevelCelebration.cs
+++ Assets/EndLevelCelebration.cs
@@ -5,9 +5,10 @@ public class EndLevelCelebration : MonoBehaviour {
     public AudioClip endClip;
     public AudioSource gameMusic;
     public RectTransform canvas;
+    BaseControllerKong baseControllerKong;
 	// Use this for initialization
 	void Start () {
-        
+        baseControllerKong = transform.parent.GetComponent<BaseControllerKong>();
 	}
 	
 	// Update is called once per frame
@@ -20,15 +21,24 @@ public class EndLevelCelebration : MonoBehaviour {
 
         if (ball.gameObject.tag == "ball")
         {
-            Invoke("ChangeMusic",3.0f);
+            //disable pauseText for the current platform
+            int thisPlatform = baseControllerKong.platform - 1;
+            transform.parent.parent.GetChild(1).GetChild(thisPlatform).GetChild(3).gameObject.SetActive(false);
+            //activate credits
+            canvas.gameObject.SetActive(true);
+            canvas.transform.GetChild(4).gameObject.SetActive(true);
+            Invoke("ChangeMusic",1.0f);
         }
 
     }
 
     void ChangeMusic() {
+
+
         gameMusic.clip = endClip;
         gameMusic.Play();
-        canvas.gameObject.SetActive(true);
-        canvas.transform.GetChild(4).gameObject.SetActive(true);
+
+
+        
     }
 }
