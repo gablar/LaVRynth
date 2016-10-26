e5989be80b839146c5662a98eb5bd8da93f9de17
gablar <g.larriuz@gmail.com>
2016-10-26 09:08:41 -0400

    Last changes for coursera

---
+++ Assets/WelcomeButtonController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WelcomeButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnButtonClicked() {
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(1);
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+}
